241180b4a395aef3acae26797c268e42db7546e2
Michael Morris <michaelmorris121@yahoo.com>
2025-12-22 13:52:26 -0600

    update display book function for better layout
    each book card to show title author pages and
    status clearly and organized

--- javascript.js
+++ javascript.js
@@ -13,25 +13,35 @@ function addToLibrary(title, author, pages, read) {
     myLibrary.push(newBook);
 };
 
-addToLibrary('harry', 'potter', '300', 'yes');
-addToLibrary('wizard', 'macbeth', '500', 'no');
+addToLibrary('A Tale of Two Cities', 'Charles Dickens', '380', 'Yes');
+addToLibrary('The Alchemist', 'Paulo Coelho', '163', 'No');
 
 function displayBook() {
-    const display = document.querySelector('.book-list');
+    const display = document.querySelector('.book-card');
 
     display.textContent = '';
 
     myLibrary.forEach(book => {
-        const books = document.createElement('div');
-        books.classList.add('book-card');
+        const bookTitle = document.createElement('h2');
+        bookTitle.classList.add('title');
+        bookTitle.textContent = `Title: ${book.title}`;
 
-        books.textContent = `
-        Title: ${book.title} 
-        Author: ${book.author} 
-        Pages: ${book.pages} 
-        Read: ${book.read}`;
+        const bookAuthor = document.createElement('p');
+        bookAuthor.classList.add('author');
+        bookAuthor.textContent = `Author: ${book.author}`;
 
-        display.appendChild(books);
+        const bookPages = document.createElement('p');
+        bookPages.classList.add('pages');
+        bookPages.textContent = `Pages: ${book.pages}`;
+
+        const bookStatus = document.createElement('p');
+        bookStatus.classList.add('read');
+        bookStatus.textContent = `Read: ${book.read}`;
+
+        display.appendChild(bookTitle);
+        display.appendChild(bookAuthor);
+        display.appendChild(bookPages);
+        display.appendChild(bookStatus);
     });
 };
 
